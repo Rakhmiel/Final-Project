c57942eacd5bd3391462fc0b498cd9184063cd72
ryandobron <ryandobron@gmail.com>
2026-04-23 18:14:18 -0400

    did a thing

---
+++ finalProject/src/main/java/edu/yu/cs/com1320/project/HashSet/HashTableImpl.java
@@ -0,0 +1,184 @@
+package edu.yu.cs.com1320.project.impl;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.yu.cs.com1320.project.HashTable;
+import edu.yu.cs.com1320.project.*;
+
+/**
+ * Instances of HashTable should be constructed with two type parameters, one for the type of the keys in the table and one for the type of the values
+ *
+ * @param <Key>
+ * @param <Value>
+ */
+public class HashTableImpl<Key, Value> implements HashTable<Key, Value> {
+    private Entry<Key, Value>[] dataArray;
+    private int size;
+    //Constructor
+    @SuppressWarnings("unchecked")
+    public HashTableImpl() {
+        this.dataArray = (Entry<Key, Value>[]) new Entry[5];
+    }
+    /**
+     * @param k the key whose value should be returned
+     * @return the value that is stored in the HashTable for k, or null if there is no such key in the table
+     */
+    @Override
+    public Value get(Key k) {
+        if (k == null) {
+            return null;
+        }
+        int index = hashValue(k);
+        Entry<Key, Value> current = this.dataArray[index];
+        while (current != null) {
+            if (current.key.equals(k)) {
+                return current.value;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    /**
+     * @param k the key at which to store the value
+     * @param v the value to store
+     *          To delete an entry, put a null value.
+     * @return if the key was already present in the HashTable, return the previous value stored for the key. If the key was not already present, return null.
+     */
+    @Override
+    public Value put(Key key, Value value) {
+        int index = hashValue(key);
+        Entry<Key, Value> current = this.dataArray[index];
+        Entry<Key, Value> previous = null;
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                Value oldValue = current.value;
+                if (value == null) {
+                        //deletes it
+                        if (previous == null) {
+                            this.dataArray[index] = current.next;
+                        }
+                        else {
+                            previous.next = current.next;
+                        }
+                        this.size--;
+                    //overwrite an old value
+                    } else {
+                        current.value = value;
+                    }
+                    return oldValue;
+                }
+                //complete overwrite
+                previous = current;
+                current = current.next;
+            }
+            //if the key isnt found
+            if (value == null) {
+                return null;
+            }
+            if (this.size >= dataArray.length) {
+                resize();
+                index = hashValue(key);
+            }
+            this.dataArray[index] = new Entry<>(key, value, this.dataArray[index]);
+            this.size++;
+            return null;
+        }
+
+    /**
+     * @param key the key whose presence in the hashtabe we are inquiring about
+     * @return true if the given key is present in the hashtable as a key, false if not
+     * @throws NullPointerException if the specified key is null
+     */
+    @Override
+    public boolean containsKey(Key key) {
+        if (key == null) {
+            throw new NullPointerException();
+        }
+        return get(key) != null;
+    }
+
+    /**
+     * @return an unmodifiable set of all the keys in this HashTable
+     * @see java.util.Collections#unmodifiableSet(Set)
+     */
+    @Override
+    public Set<Key> keySet() {
+        Set<Key> keys = new HashSet<>();
+        for (int i = 0; i < dataArray.length; i++) {
+            Entry current = dataArray[i];
+            while (current != null) {
+                keys.add((Key) current.key);
+                current = current.next;
+            }
+        }
+        return Collections.unmodifiableSet(keys);
+    }
+
+    /**
+     * @return an unmodifiable collection of all the values in this HashTable
+     * @see java.util.Collections#unmodifiableCollection(Collection)
+     */
+    @Override
+    public Collection<Value> values() {
+        Set<Value> values = new HashSet<>();
+        for (int i = 0; i < dataArray.length; i++) {
+            Entry current = dataArray[i];
+            while (current != null) {
+                values.add((Value) current.value);
+                current = current.next;
+            }
+        }
+        return Collections.unmodifiableSet(values);
+    }
+
+    /**
+     * @return how entries there currently are in the HashTable
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+    //resizing
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        Entry<Key, Value>[] oldArray = this.dataArray;
+        Entry<Key, Value>[] newArray = (Entry<Key, Value>[]) new Entry[oldArray.length * 2];
+
+        this.dataArray = newArray;
+
+        for (Entry<Key, Value> bucket : oldArray) {
+            Entry<Key, Value> current = bucket;
+
+            while (current != null) {
+                Entry<Key, Value> next = current.next;
+                int index = hashValue(current.key);
+                current.next = newArray[index];
+                newArray[index] = current;
+                current = next;
+            }
+        }
+
+    }
+
+    private int hashValue(Key key) {
+        return Math.abs(key.hashCode() % this.dataArray.length);
+    }
+    //the linked list class
+   /* private static class Entry<Key, Value> {
+        private Key key;
+        private Value value;
+        Entry<Key, Value> next;
+
+        public Entry(Key key, Value value, Entry<Key, Value> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+    }
+}
+    */
+}
--- finalProject/src/main/java/edu/yu/cs/com1320/project/majorObject.java
+++ finalProject/src/main/java/edu/yu/cs/com1320/project/majorObject.java
@@ -1,23 +1,47 @@
+import java.io.IOException;
 import java.util.*;
+
 import edu.yu.cs.com1320.project.*;
+
 public class majorObject implements Comparable<Object> {
     private String major;
     private double totalSalary;
     private double averageSalary;
     private personObject[] = new personObject[];
 
-    public majorObject(Double salary, String major) {
-        this.major = major;
-        this.totalSalary += salary;
+    public majorObject(personObject person) {
+        this.major = personObject.getMajor();
+        this.totalSalary += personObject.getSalary();
     }
+    public add(personObject person) throws IOException {
+        if (person.getMajor() != major) {
+            throw new IOException("Incorrect major.");
+        }
+    }
+
     @Override
     //compares the salaries
     public int compareTo(Object Other) {
-        return (int) this.getSalary() - (personObject) Other.getSalary();
+        return (int) this.getMajor() - (personObject) Other.getMajor();
     }
     @Override
     //hashes them based on their major
     public int hashCode() {
         return Objects.hash(major);
+    }
+    @Override
+    //checks if two majors are equal
+    public boolean equals(Object other) {
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        if (this == other) {
+            return true;
+        }
+        other = (majorObject) other;
+        if (this.major() == other.major()) {
+            return true;
+        }
+        return false;
     }
 }
