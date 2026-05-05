5a01a8853d6bd099a3058e74175c2cd7389490ca
ryandobron <ryandobron@gmail.com>
2026-05-05 18:11:57 -0400

    fixed gitignore and other stuff

--- src/main/java/finalProject/comparisonProgram.java
+++ src/main/java/finalProject/comparisonProgram.java
@@ -1,22 +1,11 @@
 package finalProject;
 
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
-
 import finalProject.HashSet.*;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.lang.reflect.Type;
-import java.net.URI;
-import java.util.Base64;
-import java.util.HashMap;
 
 public class comparisonProgram {
-    private HashTableImpl storage;
     public static void main(String[] args) {
         int majors = Integer.parseInt(args[0]);
-        storage = new HashTableImpl(majors);
+        HashTableImpl storage = new HashTableImpl<>(majors);
     }
 
 
